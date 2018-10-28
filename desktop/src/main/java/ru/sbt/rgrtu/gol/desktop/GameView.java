40c036e171912444d79e14b9e8223376aa320c2b
Kostik Todorenko <kostya.todorenko@mail.ru>
2018-10-28 13:04:53 +0300

    Add swing presentation

---
+++ desktop/src/main/java/ru/sbt/rgrtu/gol/desktop/GameView.java
@@ -0,0 +1,34 @@
+package ru.sbt.rgrtu.gol.desktop;
+
+import ru.sbt.rgrtu.gol.cli.game.Gol;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+public class GameView extends JPanel {
+
+    private Gol gol;
+    private Image image;
+
+    public GameView(Gol gol) {
+        this.gol = gol;
+        try {
+            image = ImageIO.read(new File("cli//src//main//resources//human.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        for (int y = 0; y < gol.getSizeY(); y++) {
+            for (int x = 0; x < gol.getSizeX(); x++) {
+                if (gol.getPoint(x, y))
+                    g.drawImage(image, x * 20, y * 40, null);
+            }
+        }
+    }
+}
--- desktop/src/main/java/ru/sbt/rgrtu/gol/desktop/SwingPresentation.java
+++ desktop/src/main/java/ru/sbt/rgrtu/gol/desktop/SwingPresentation.java
@@ -9,33 +9,22 @@ import java.awt.*;
 
 public class SwingPresentation implements Presentation {
     private JFrame frame;
-    private Gol gol;
-    private TextArea textArea;
     private SwingPresentationListener swingPresetnationListener;
+    private GameView gameView;
 
 
     public SwingPresentation(Gol gol) {
-        this.gol = gol;
         swingPresetnationListener = new SwingPresentationListener();
+        gameView = new GameView(gol);
         frame = new JFrame();
-        textArea = new TextArea();
-        frame.setBounds(111, 111, 500, 400);
-        frame.add(textArea);
+        frame.setBounds(111, 111, 1200, 600);
+        frame.add(gameView);
         frame.addWindowListener(swingPresetnationListener);
         frame.setVisible(true);
     }
 
     @Override
     public void show() {
-        StringBuilder out = new StringBuilder();
-        out.append(String.format("===== %1$05d =====", gol.getGeneration())).append("\n");
-        for (int y = 0; y < gol.getSizeY(); y++) {
-            for (int x = 0; x < gol.getSizeX(); x++) {
-                out.append(gol.getPoint(x, y) ? "@" : " ");
-            }
-            out.append("\n");
-        }
-        textArea.setText(out.toString());
         frame.repaint();
     }
 }
